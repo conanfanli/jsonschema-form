1d25af770289e0d48755bc95d36616e07868841b
Conan Li <conan@junipersquare.com>
2023-10-09 16:19:10 -0400

    works

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Form from '@rjsf/mui';
+import { RJSFSchema } from '@rjsf/utils';
+import validator from '@rjsf/validator-ajv8';
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,4 +26,72 @@ function App() {
   );
 }
 
-export default App;
+
+/*
+const schema: RJSFSchema = {
+title: 'Test form',
+type: 'string',
+};
+*/
+
+
+const schema: RJSFSchema = {
+  "$defs": {
+    "Property": {
+      "properties": {
+        "name": {
+          "title": "Name",
+          "type": "string"
+        },
+        "attribute_type": {
+          "enum": [
+            "N",
+            "S"
+          ],
+          "title": "Attribute Type",
+          "type": "string"
+        },
+        "key_type": {
+          "default": null,
+          "enum": [
+            "HASH",
+            "RANGE",
+            null
+          ],
+          "title": "Key Type"
+        }
+      },
+      "required": [
+        "name",
+        "attribute_type"
+      ],
+      "title": "Property",
+      "type": "object"
+    }
+  },
+  "properties": {
+    "name": {
+      "title": "Name",
+      "type": "string"
+    },
+    "properties": {
+      "type": "array",
+      "items": {
+        "$ref": "#/$defs/Property"
+      },
+      "minItems": 1,
+      "title": "Properties"
+    }
+  },
+  "required": [
+    "name",
+    "properties"
+  ],
+  "title": "TableDef",
+  "type": "object"
+}
+function MyForm() {
+  return <Form schema={schema} validator={validator} />
+}
+
+export default MyForm;
