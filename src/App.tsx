f4c9a87ad89a98f2befab165f2ddb2bfc7e0f5f9
Conan Li <conan@junipersquare.com>
2023-10-09 16:22:36 -0400

    github

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,7 @@ import React from 'react';
 import Form from '@rjsf/mui';
 import { RJSFSchema } from '@rjsf/utils';
 import validator from '@rjsf/validator-ajv8';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-
-/*
-const schema: RJSFSchema = {
-title: 'Test form',
-type: 'string',
-};
-*/
 
 
 const schema: RJSFSchema = {
