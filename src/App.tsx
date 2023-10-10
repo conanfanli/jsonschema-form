6b7de7d982b9209b1de1d8bfaf13c2412e89e5c9
Conan Li <conan@junipersquare.com>
2023-10-10 13:31:25 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -8,53 +8,66 @@ import { Box, Button, TextField } from "@mui/material";
 import './App.css';
 
 
-const defaultSchema: RJSFSchema = {
-  type: 'object',
-  properties: {
-    schemaUrl: {
-      type: 'string',
-    },
-    schemaDefinition: {
-      type: 'string',
-    },
-    submitUrl: {
-      type: 'string',
-    },
-  },
-}
 
 
 function SchemaPicker() {
   const [searchParams, setSearchParams] = useSearchParams()
-  const [url, setUrl] = React.useState(searchParams.get("schema_url") || "")
+
+  const [schemaUrl, setSchemaUrl] = React.useState(searchParams.get("schema_url") || "")
+  const [submitUrl, setSubmitUrl] = React.useState(searchParams.get("submit_url") || "")
+  // const [submitUrl, setSubmitUrl] = React.useState(searchParams.get("submit_url") || "")
+
   const [schema, setSchema] = React.useState(null)
 
 
   return (
     <div>
-      <TextField onChange={(e) => { setUrl(e.target.value) }} value={url} fullWidth helperText="Schema URL" />
-      <Button
-        variant="contained"
-        onClick={async () => {
-          setSearchParams({ schema_url: url })
+      <Box noValidate component="form">
+        <TextField onChange={(e) => { setSchemaUrl(e.target.value) }} value={schemaUrl} fullWidth helperText="Schema URL" />
+        <TextField onChange={(e) => setSubmitUrl(e.target.value)} value={submitUrl} fullWidth helperText="Post to" />
+        <Button
+          variant="contained"
+          onClick={async () => {
+            setSearchParams({ schema_url: schemaUrl, submit_url: submitUrl })
 
-          const res = await fetch(url)
+            const res = await fetch(schemaUrl)
+            const body = await res.json()
+            setSchema(body)
+          }}
+        >
+          Render
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => { setSchemaUrl(""); setSearchParams({}) }}
+        >
+          clear
+        </Button>
+      </Box>
+      {schema ? <SubmissionForm schema={schema} submitUrl={submitUrl} /> : null}
+    </div>
+  )
+}
+
+function SubmissionForm({ schema, submitUrl }) {
+  const [formData, setFormData] = React.useState(null);
+  return (
+    <div>
+      <Form
+        schema={schema}
+        validator={validator}
+        formData={formData}
+        onChange={(e) => setFormData(e.formData)}
+        onSubmit={async (e) => {
+          const res = await fetch(submitUrl, {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData),
+          })
           const body = await res.json()
           console.log('body', body)
-          setSchema(body)
         }}
-        fullWidth
-      >
-        Render
-      </Button>
-      <Button
-        variant="contained"
-        onClick={() => { setUrl(""); setSearchParams({}) }}
-        fullWidth
-      >
-        clear
-      </Button>
-      {schema ? <Form schema={schema} validator={validator} /> : null}
+      />
     </div>
   )
 }
